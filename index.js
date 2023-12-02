7b763da490589b9475bf93eb8664d8bf4a591261
ikhlas-ct <ikhlasulamalmawardi@gmail.com>
2023-12-02 12:24:48 +0700

    p8

---
+++ index.js
@@ -0,0 +1,21 @@
+let makanan = ['sate','mie','roti']
+
+console.log(makanan[1])
+
+let myarr3 =['teks',2,false,'myfunc',['budi','dina','bini']];
+
+console.log(myarr3[4][1])
+
+var arr= []
+arr[0]='senin'
+arr[1]='selasa'
+arr[2]='rabu'
+console.log(arr)
+
+var arr1=['senin','selasa','gg']
+for (i=0;i<3;i++){
+    console.log(arr1[i])
+}
+
+let keluarga =['a','b','c'['d','e','f'['g','h','j','k']]]
+console.log('seorang keluarga bernama', [keluarga[1]], 'memiliki istri bernama', keluarga[3][1] )
